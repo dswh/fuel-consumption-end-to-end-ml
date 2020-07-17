2ea13517be097a28274cc16c8e552a8c64945d79
datasciencewithharshit <datasciencewithharshit@gmail.com>
2020-07-18 02:47:12 +0530

    add

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = Flask('mpg_prediction')
 def predict():
     vehicle = request.get_json()
     print(vehicle)
-    with open('model.bin', 'rb') as f_in:
+    with open('./model.bin', 'rb') as f_in:
         model = pickle.load(f_in)
         f_in.close()
     predictions = predict_mpg(vehicle, model)
