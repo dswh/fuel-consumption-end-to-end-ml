9dc94d212b6c02b6cf284e01dd3c5037c757e32d
datasciencewithharshit <datasciencewithharshit@gmail.com>
2020-07-17 23:44:31 +0530

    Add flask code

---
+++ app/main.py
@@ -0,0 +1,24 @@
+import pickle
+from flask import Flask, request, jsonify
+from ml_model import predict_mpg
+
+
+app = Flask('mpg_prediction')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    vehicle = request.get_json()
+    print(vehicle)
+    with open('model.bin', 'rb') as f_in:
+        model = pickle.load(f_in)
+        f_in.close()
+    predictions = predict_mpg(vehicle, model)
+
+    result = {
+        'mpg_prediction': list(predictions)
+    }
+    return jsonify(result)
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    return "Pinging Model!!"
---
+++ app/ml_model.py
@@ -0,0 +1,79 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+
+
+##functions
+
+
+def preprocess_origin_cols(df):
+    df["Origin"] = df["Origin"].map({1: "India", 2: "USA", 3: "Germany"})
+    return df
+
+
+acc_ix, hpower_ix, cyl_ix = 3, 5, 1
+
+class CustomAttrAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, acc_on_power=True): # no *args or **kargs
+        self.acc_on_power = acc_on_power
+    def fit(self, X, y=None):
+        return self  # nothing else to do
+    def transform(self, X):
+        acc_on_cyl = X[:, acc_ix] / X[:, cyl_ix]
+        if self.acc_on_power:
+            acc_on_power = X[:, acc_ix] / X[:, hpower_ix]
+            return np.c_[X, acc_on_power, acc_on_cyl]
+        
+        return np.c_[X, acc_on_cyl]
+
+
+def num_pipeline_transformer(data):
+    numerics = ['float64', 'int64']
+
+    num_attrs = data.select_dtypes(include=numerics)
+
+    num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy="median")),
+        ('attrs_adder', CustomAttrAdder()),
+        ('std_scaler', StandardScaler()),
+        ])
+    return num_attrs, num_pipeline
+
+
+def pipeline_transformer(data):
+    
+    cat_attrs = ["Origin"]
+    num_attrs, num_pipeline = num_pipeline_transformer(data)
+    
+    full_pipeline = ColumnTransformer([
+        ("num", num_pipeline, list(num_attrs)),
+        ("cat", OneHotEncoder(), cat_attrs),
+        ])
+    full_pipeline.fit_transform(data)
+    return full_pipeline    
+
+
+def predict_mpg(config, model):
+    
+    if type(config) == dict:
+        df = pd.DataFrame(config)
+    else:
+        df = config
+    
+    preproc_df = preprocess_origin_cols(df)
+    print(preproc_df)
+    pipeline = pipeline_transformer(preproc_df)
+    prepared_df = pipeline.transform(preproc_df)
+    print(len(prepared_df[0]))
+    y_pred = model.predict(prepared_df)
+    return y_pred
